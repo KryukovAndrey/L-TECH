642306b8faef347d415abbcec7174bed090d009f
KryukovAndrey <kryukov.ios@yandex.ru>
2022-10-27 17:34:04 +0300

    Initial Commit

---
+++ L-TECH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L-TECH
+//
+//  Created by Andrey Kryukov on 27.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
